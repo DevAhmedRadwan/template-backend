701385539c787f4b88de89f4b302d064681f2d3f
ahmed radwan <DevAhmedRadwan@gmail.com>
2022-02-03 17:23:51 +0200

    removed some unused constants

--- src/constants/models-names.js
+++ src/constants/models-names.js
@@ -1,6 +1,1 @@
 module.exports.USER = "user";
-module.exports.ORGANIZATION = "organization";
-module.exports.PROJECT = "project";
-module.exports.BOARD = "board";
-module.exports.TASK = "task";
-module.exports.COMMENT = "comment";
